a45486fe5ae65e35535f2f05c9331ae735c0902d
노소래 <yessorae@gmail.com>
2023-06-05 13:16:23 +0900

    build: 힐트 앱에 설치

--- app/src/main/java/com/yessorae/goalreminder/GoalReminderApplication.kt
+++ app/src/main/java/com/yessorae/goalreminder/GoalReminderApplication.kt
@@ -1,7 +1,7 @@
 package com.yessorae.goalreminder
 
 import android.app.Application
-//import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
-//@AndroidEntryPoint
+@AndroidEntryPoint
 class GoalReminderApplication : Application()
