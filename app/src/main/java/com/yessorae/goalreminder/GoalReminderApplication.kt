b11c375b1393935a212074ede4e978fabbee4331
노소래 <yessorae@gmail.com>
2023-06-05 13:15:25 +0900

    build: 빌드환경 세팅, 힐트 디펜던시 추가

---
+++ app/src/main/java/com/yessorae/goalreminder/GoalReminderApplication.kt
@@ -0,0 +1,7 @@
+package com.yessorae.goalreminder
+
+import android.app.Application
+//import dagger.hilt.android.AndroidEntryPoint
+
+//@AndroidEntryPoint
+class GoalReminderApplication : Application()
