131d1a7815ec2f869d934ad250664c38ddf6fb3e
노소래 <yessorae@gmail.com>
2023-06-09 01:29:43 +0900

    feat: 뷰모델 메서드 작성

--- presentation/src/main/java/com/yessorae/presentation/screen/editors/todo/TodoEditorScreen.kt
+++ presentation/src/main/java/com/yessorae/presentation/screen/editors/todo/TodoEditorScreen.kt
@@ -1,8 +1,13 @@
 package com.yessorae.presentation.screen.editors.todo
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun TodoEditorScreen() {
-
+fun TodoEditorScreen(
+    viewModel: TodoEditorViewModel = hiltViewModel()
+) {
+    val model = viewModel.state.collectAsState()
+    // todo UI
 }
--- presentation/src/main/java/com/yessorae/presentation/screen/editors/todo/TodoEditorViewModel.kt
+++ presentation/src/main/java/com/yessorae/presentation/screen/editors/todo/TodoEditorViewModel.kt
@@ -9,6 +9,135 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TodoEditorViewModel @Inject constructor() : BaseScreenViewModel<TodoEditorScreenState>() {
+
+    fun onChangeTitle(title: String) {
+        updateState {
+            stateValue.copy(
+                title = title
+            )
+        }
+
+    }
+
+    fun onClickStartDate() {
+        updateState {
+            stateValue.copy(
+                todoEditorDialogState = TodoEditorDialogState.StartDate
+            )
+        }
+    }
+
+    fun onClickEndDate() {
+        updateState {
+            stateValue.copy(
+                todoEditorDialogState = TodoEditorDialogState.EndDate
+            )
+        }
+    }
+
+    fun onClickStartTime() {
+        updateState {
+            stateValue.copy(
+                todoEditorDialogState = TodoEditorDialogState.StartTime
+            )
+        }
+    }
+
+    fun onClickEndTime() {
+        updateState {
+            stateValue.copy(
+                todoEditorDialogState = TodoEditorDialogState.EndTime
+            )
+        }
+    }
+
+    fun onSelectDate(date: LocalDate) {
+        updateState {
+            when (stateValue.todoEditorDialogState) {
+                TodoEditorDialogState.StartDate -> {
+                    stateValue.copy(
+                        startDate = date
+                    )
+                }
+
+                TodoEditorDialogState.EndDate -> {
+                    stateValue.copy(
+                        endDate = date
+                    )
+                }
+
+                else -> stateValue
+            }
+        }
+        onCloseDialog()
+    }
+
+    fun onSelectTime(time: LocalDateTime) {
+        updateState {
+            when (stateValue.todoEditorDialogState) {
+                TodoEditorDialogState.StartTime -> {
+                    stateValue.copy(
+                        startTime = time
+                    )
+                }
+
+                TodoEditorDialogState.EndTime -> {
+                    stateValue.copy(
+                        endTime = time
+                    )
+                }
+
+                else -> stateValue
+            }
+        }
+    }
+
+    fun onClickContributeGoal() {
+        updateState {
+            stateValue.copy(
+                todoEditorDialogState = TodoEditorDialogState.ContributeGoal
+            )
+        }
+    }
+
+    fun onSelectContributeGoal(goal: Goal) {
+        updateState {
+            stateValue.copy(
+                contributeGoal = goal
+            )
+        }
+    }
+
+    fun onChangeContributeGoalScore(score: Int) {
+        updateState {
+            stateValue.copy(
+                contributeScore = score
+            )
+        }
+    }
+
+    fun onClickBack() {
+        // todo 다시 안보기 조건 따져서 하자
+        updateState {
+            stateValue.copy(
+                todoEditorDialogState = TodoEditorDialogState.ExitConfirm
+            )
+        }
+    }
+
+    fun onCloseDialog() {
+        updateState {
+            stateValue.copy(
+                todoEditorDialogState = TodoEditorDialogState.None
+            )
+        }
+    }
+
+    fun onClickSave() {
+        // todo insert or update TODO
+    }
+
+
     override fun createInitialState(): TodoEditorScreenState {
         return TodoEditorScreenState()
     }
@@ -32,5 +161,9 @@ sealed class TodoEditorDialogState {
     object StartTime : TodoEditorDialogState()
     object EndDate : TodoEditorDialogState()
     object EndTime : TodoEditorDialogState()
+    object ContributeGoal : TodoEditorDialogState()
+
+    object ExitConfirm : TodoEditorDialogState()
+
 
 }
