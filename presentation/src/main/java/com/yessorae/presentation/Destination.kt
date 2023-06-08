236eaba272b9a4327486b7ea5713f47802fff812
노소래 <yessorae@gmail.com>
2023-06-09 01:10:21 +0900

    feat: Destination 정리

--- presentation/src/main/java/com/yessorae/presentation/Destination.kt
+++ presentation/src/main/java/com/yessorae/presentation/Destination.kt
@@ -7,3 +7,19 @@ interface Destination {
 object MainDestination : Destination {
     override val route = "main"
 }
+
+object TodoEditorDestination : Destination {
+    override val route: String = "todo_editor"
+}
+
+object GoalEditorDestination : Destination {
+    override val route: String = "goal_editor"
+}
+
+object SettingDestination : Destination {
+    override val route: String = "setting"
+}
+
+object RepeatedTodoEditorDestination : Destination {
+    override val route: String = "repeated_todo_editor_destination"
+}
