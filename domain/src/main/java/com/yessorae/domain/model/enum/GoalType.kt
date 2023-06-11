bea7a08c7323fb04a651e8af211a9e8419c492ba
노소래 <yessorae@gmail.com>
2023-06-12 00:04:06 +0900

    feat: GoalType 통합

--- domain/src/main/java/com/yessorae/domain/model/enum/GoalType.kt
+++ domain/src/main/java/com/yessorae/domain/model/enum/GoalType.kt
@@ -3,5 +3,15 @@ package com.yessorae.domain.model.enum
 enum class GoalType {
     YEARLY,
     WEEKLY,
-    MONTHLY
+    MONTHLY,
+    NONE
+}
+
+fun String.toGoalType(): GoalType {
+    return when (this) {
+        GoalType.YEARLY.name -> GoalType.YEARLY
+        GoalType.WEEKLY.name -> GoalType.WEEKLY
+        GoalType.MONTHLY.name -> GoalType.MONTHLY
+        else -> GoalType.NONE
+    }
 }
