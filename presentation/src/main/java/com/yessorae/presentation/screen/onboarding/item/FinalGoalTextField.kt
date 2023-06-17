1a345b76d196334166d365e004227220b8a16e48
노소래 <yessorae@gmail.com>
2023-06-17 18:21:55 +0900

    feat: 최종목표 텍스트 필드 추가

---
+++ presentation/src/main/java/com/yessorae/presentation/screen/onboarding/item/FinalGoalTextField.kt
@@ -0,0 +1,73 @@
+package com.yessorae.presentation.screen.onboarding.item
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Divider
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.yessorae.presentation.FreeTextField
+import com.yessorae.presentation.R
+import com.yessorae.presentation.ext.keyboardAsState
+
+@Composable
+fun FinalGoalTextField(
+    modifier: Modifier = Modifier,
+    title: String,
+    onChangeTitle: (String) -> Unit = {},
+    placeholderText: String,
+    textStyle: TextStyle,
+    singleLine: Boolean = true
+) {
+    var cursorColor by remember {
+        mutableStateOf(Color.Transparent)
+    }
+
+    val isKeyboardOpen by keyboardAsState()
+
+    cursorColor = if (isKeyboardOpen) {
+        MaterialTheme.colorScheme.onBackground
+    } else {
+        Color.Transparent
+    }
+
+    Column {
+        FreeTextField(
+            value = title,
+            onValueChange = {
+                onChangeTitle(it)
+            },
+            modifier = modifier,
+            textStyle = textStyle.copy(fontWeight = FontWeight.Bold),
+            placeholder = {
+                Text(text = placeholderText, style = textStyle)
+            },
+            singleLine = singleLine,
+            colors = TextFieldDefaults.colors(
+                disabledIndicatorColor = MaterialTheme.colorScheme.background,
+                focusedIndicatorColor = MaterialTheme.colorScheme.background,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.background,
+                focusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                cursorColor = cursorColor
+            ),
+            textColor = MaterialTheme.colorScheme.onBackground
+        )
+
+    }
+
+}
