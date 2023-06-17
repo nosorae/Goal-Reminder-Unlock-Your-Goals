5537dab3c412d82d7626172a63da6a98633b0ec6
노소래 <yessorae@gmail.com>
2023-06-17 18:37:40 +0900

    feat: 최종목표 전용 탑앱바 추가

---
+++ presentation/src/main/java/com/yessorae/presentation/screen/onboarding/item/FinalGoalTopAppBar.kt
@@ -0,0 +1,29 @@
+package com.yessorae.presentation.screen.onboarding.item
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FinalGoalTopAppBar(
+    title: String,
+    onClickBack: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        navigationIcon = {
+            IconButton(onClick = { onClickBack() }) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
+}
