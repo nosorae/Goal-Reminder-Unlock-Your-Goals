041159cbb7340f84ebdd91c5dcad2eb864577362
노소래 <yessorae@gmail.com>
2023-06-19 23:55:01 +0900

    feat: getTodoWithGoalFlowUseCase 추가

--- data/src/main/java/com/yessorae/data/repository/TodoRepositoryImpl.kt
+++ data/src/main/java/com/yessorae/data/repository/TodoRepositoryImpl.kt
@@ -1,33 +1,52 @@
 package com.yessorae.data.repository
 
+import com.yessorae.common.Logger
+import com.yessorae.data.local.database.dao.TodoDao
+import com.yessorae.data.local.database.model.asDomainModel
+import com.yessorae.data.local.database.model.asEntity
 import com.yessorae.domain.model.Todo
-import com.yessorae.domain.model.TodoWithGoal
-import com.yessorae.domain.model.mockTodoData
-import com.yessorae.domain.model.mockTodoWithGoalData
 import com.yessorae.domain.repository.TodoRepository
+import com.yessorae.util.toEndLocalDateTime
+import com.yessorae.util.toStartLocalDateTime
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.atTime
 
-class TodoRepositoryImpl @Inject constructor() : TodoRepository {
-    override fun getDailyTodosFlow(day: LocalDateTime): Flow<List<Todo>> { // todo replace
-        return flowOf(mockTodoData) // todo replace
+class TodoRepositoryImpl @Inject constructor(
+    private val todoDao: TodoDao
+) : TodoRepository {
+    override fun getDailyTodosFlow(day: LocalDateTime): Flow<List<Todo>> {
+        val startDateTime = day.date.toStartLocalDateTime()
+        val endDateTime = day.date.toEndLocalDateTime()
+        Logger.uiDebug("getDailyTodosFlow / startDateTime $startDateTime / endDateTime $endDateTime")
+        return todoDao.loadTodoFlow(
+            start = startDateTime,
+            end = endDateTime
+        ).map { list ->
+            Logger.uiDebug("getDailyTodosFlow / list $list")
+            list.map { todo ->
+                todo.asDomainModel()
+            }
+        }
     }
 
     override suspend fun getTodo(todoId: Int): Todo {
-        return mockTodoData[0] // todo replace
+        return todoDao.loadTodoById(id = todoId).asDomainModel()
     }
 
     override suspend fun insertTodo(todo: Todo): Int {
-        return 0 // todo impl
+        return todoDao.insert(todo.asEntity()).toInt()
     }
 
     override suspend fun updateTodo(todo: Todo) {
-        // todo impl
+        Logger.uiDebug("data updateTodo $todo")
+        todoDao.update(todo.asEntity())
     }
 
-    override suspend fun deleteTodo(todoId: Int) {
-        // todo impl
+    override suspend fun deleteTodo(todo: Todo) {
+        Logger.uiDebug("data deleteTodo $todo")
+        todoDao.delete(todo.asEntity())
     }
 }
---
+++ domain/src/main/java/com/yessorae/domain/usecase/GetTodoWithUpperGoalUseCaseFlow.kt
@@ -0,0 +1,36 @@
+package com.yessorae.domain.usecase
+
+import com.yessorae.domain.model.TodoWithGoal
+import com.yessorae.domain.repository.GoalRepository
+import com.yessorae.domain.repository.TodoRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.datetime.LocalDateTime
+import javax.inject.Inject
+
+class GetTodoWithUpperGoalUseCaseFlow @Inject constructor(
+    private val todoRepository: TodoRepository,
+    private val goalRepository: GoalRepository
+) {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(date: LocalDateTime): Flow<List<TodoWithGoal>> {
+        return todoRepository.getDailyTodosFlow(date).flatMapLatest { list ->
+            flow {
+                emit(
+                    list.map { todo ->
+                        todo.upperGoalId?.let { upperGoalId ->
+                            val upperGoal = goalRepository.getGoalById(goalId = upperGoalId)
+                            TodoWithGoal(
+                                todo = todo,
+                                upperGoal = upperGoal
+                            )
+                        } ?: TodoWithGoal(todo = todo)
+                    }
+                )
+            }
+        }
+    }
+}
