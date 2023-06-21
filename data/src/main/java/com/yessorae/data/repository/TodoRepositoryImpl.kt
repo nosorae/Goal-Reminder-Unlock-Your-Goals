052018189755a640a4ec85d42e25cfdb14175a22
노소래 <yessorae@gmail.com>
2023-06-21 23:59:17 +0900

    chore: 주석제거

--- data/src/main/java/com/yessorae/data/repository/TodoRepositoryImpl.kt
+++ data/src/main/java/com/yessorae/data/repository/TodoRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.yessorae.data.repository
 
-import com.yessorae.common.Logger
 import com.yessorae.data.local.database.dao.TodoDao
 import com.yessorae.data.local.database.model.asDomainModel
 import com.yessorae.data.local.database.model.asEntity
@@ -12,7 +11,6 @@ import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.atTime
 
 class TodoRepositoryImpl @Inject constructor(
     private val todoDao: TodoDao
@@ -20,12 +18,10 @@ class TodoRepositoryImpl @Inject constructor(
     override fun getDailyTodosFlow(day: LocalDateTime): Flow<List<Todo>> {
         val startDateTime = day.date.toStartLocalDateTime()
         val endDateTime = day.date.toEndLocalDateTime()
-        Logger.uiDebug("getDailyTodosFlow / startDateTime $startDateTime / endDateTime $endDateTime")
         return todoDao.loadTodoFlow(
             start = startDateTime,
             end = endDateTime
         ).map { list ->
-            Logger.uiDebug("getDailyTodosFlow / list $list")
             list.map { todo ->
                 todo.asDomainModel()
             }
@@ -41,12 +37,10 @@ class TodoRepositoryImpl @Inject constructor(
     }
 
     override suspend fun updateTodo(todo: Todo) {
-        Logger.uiDebug("data updateTodo $todo")
         todoDao.update(todo.asEntity())
     }
 
     override suspend fun deleteTodo(todo: Todo) {
-        Logger.uiDebug("data deleteTodo $todo")
         todoDao.delete(todo.asEntity())
     }
 }
