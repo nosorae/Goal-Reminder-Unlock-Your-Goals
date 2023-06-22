7d18e822f5840e371b35912e562b3178309d1372
노소래 <yessorae@gmail.com>
2023-06-22 23:32:30 +0900

    feat: todo done 기능 Transaction 추가

--- data/src/main/java/com/yessorae/data/local/database/dao/TodoDao.kt
+++ data/src/main/java/com/yessorae/data/local/database/dao/TodoDao.kt
@@ -2,9 +2,15 @@ package com.yessorae.data.local.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import com.yessorae.data.Constants.COL_GOAL_ID
 import com.yessorae.data.Constants.COL_TODO_ID
+import com.yessorae.data.Constants.TABLE_GOAL
 import com.yessorae.data.Constants.TABLE_TODO
+import com.yessorae.data.local.database.model.GoalEntity
 import com.yessorae.data.local.database.model.TodoEntity
+import com.yessorae.domain.model.Goal
 import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.LocalDateTime
 
@@ -18,4 +24,25 @@ abstract class TodoDao : BaseDao<TodoEntity> {
         start: LocalDateTime,
         end: LocalDateTime
     ): Flow<List<TodoEntity>>
+
+
+    @Update
+    abstract fun updateUpperGoal(upperGoal: GoalEntity)
+
+    @Transaction
+    suspend fun checkTodo(todoEntity: TodoEntity, goalEntity: GoalEntity?) {
+        val newDone = todoEntity.done.not()
+        update(entity = todoEntity.copy(done = newDone))
+        goalEntity?.let {
+            updateUpperGoal(
+                upperGoal = goalEntity.copy(
+                    currentScore = goalEntity.currentScore + if (newDone) {
+                        todoEntity.upperGoalContributionScore ?: 0
+                    } else {
+                        -(todoEntity.upperGoalContributionScore ?: 0)
+                    }
+                )
+            )
+        }
+    }
 }
