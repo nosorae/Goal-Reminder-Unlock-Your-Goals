c3303ed6e369ef3a41dbda222a72086e83c59f78
노소래 <yessorae@gmail.com>
2023-06-23 00:01:06 +0900

    feat: 목표삭제 Transaction 추가

--- data/src/main/java/com/yessorae/data/local/database/dao/GoalDao.kt
+++ data/src/main/java/com/yessorae/data/local/database/dao/GoalDao.kt
@@ -3,21 +3,25 @@ package com.yessorae.data.local.database.dao
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
 import com.yessorae.data.Constants.COL_DATE_FROM
 import com.yessorae.data.Constants.COL_GOAL_ID
+import com.yessorae.data.Constants.COL_UPPER_GOAL_ID
 import com.yessorae.data.Constants.TABLE_GOAL
+import com.yessorae.data.Constants.TABLE_TODO
 import com.yessorae.data.local.database.model.GoalEntity
+import com.yessorae.data.local.database.model.TodoEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.LocalDateTime
 
 @Dao
-abstract class GoalDao : BaseDao<GoalEntity> {
+interface GoalDao : BaseDao<GoalEntity> {
     @Query(
         value = """
             SELECT * FROM $TABLE_GOAL WHERE $COL_DATE_FROM BETWEEN :start AND :end AND type = :goalType
         """
     )
-    abstract fun loadGoalsFlow(
+    fun loadGoalsFlow(
         start: LocalDateTime,
         end: LocalDateTime,
         goalType: String
@@ -28,5 +32,48 @@ abstract class GoalDao : BaseDao<GoalEntity> {
             SELECT * FROM $TABLE_GOAL WHERE $COL_GOAL_ID = :id 
         """
     )
-    abstract suspend fun loadGoalById(id: Int): GoalEntity
+    suspend fun loadGoalById(id: Int): GoalEntity
+
+    @Query(
+        value = """
+            SELECT * FROM $TABLE_TODO WHERE $COL_UPPER_GOAL_ID = :goalId
+        """
+    )
+    suspend fun loadTodosByUpperGoalId(goalId: Int): List<TodoEntity>
+
+    @Query(
+        value = """
+            SELECT * FROM $TABLE_GOAL WHERE $COL_UPPER_GOAL_ID = :goalId
+        """
+    )
+    suspend fun loadGoalsByUpperGoalId(goalId: Int): List<GoalEntity>
+
+    @Update
+    suspend fun updateTodo(todo: TodoEntity)
+
+    // todo async 고려
+    @Transaction
+    suspend fun deleteGoal(goal: GoalEntity) {
+        val goalId = goal.goalId
+
+        val todoEntities = loadTodosByUpperGoalId(goalId)
+        val goalEntities = loadGoalsByUpperGoalId(goalId)
+        todoEntities.forEach { todoEntity ->
+            updateTodo(
+                todoEntity.copy(
+                    upperGoalId = null,
+                    upperGoalContributionScore = null
+                )
+            )
+        }
+        goalEntities.forEach { goalEntity ->
+            update(
+                goalEntity.copy(
+                    upperGoalId = null,
+                    upperGoalContributionScore = null
+                )
+            )
+        }
+        delete(goal)
+    }
 }
