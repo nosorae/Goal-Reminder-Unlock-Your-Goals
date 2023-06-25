ee120b8ec8ff63b5bc33be45b0214043b0e7418c
노소래 <yessorae@gmail.com>
2023-06-26 01:10:48 +0900

    feat: 투두 업데이트, 삭제 Transaction 추가

--- data/src/main/java/com/yessorae/data/local/database/dao/TodoDao.kt
+++ data/src/main/java/com/yessorae/data/local/database/dao/TodoDao.kt
@@ -6,11 +6,11 @@ import androidx.room.Transaction
 import androidx.room.Update
 import com.yessorae.data.Constants.COL_GOAL_ID
 import com.yessorae.data.Constants.COL_TODO_ID
+import com.yessorae.data.Constants.COL_UPPER_GOAL_ID
 import com.yessorae.data.Constants.TABLE_GOAL
 import com.yessorae.data.Constants.TABLE_TODO
 import com.yessorae.data.local.database.model.GoalEntity
 import com.yessorae.data.local.database.model.TodoEntity
-import com.yessorae.domain.model.Goal
 import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.LocalDateTime
 
@@ -25,24 +25,152 @@ interface TodoDao : BaseDao<TodoEntity> {
         end: LocalDateTime
     ): Flow<List<TodoEntity>>
 
+    @Query(
+        value = """
+            SELECT * FROM $TABLE_GOAL WHERE $COL_GOAL_ID = :id 
+        """
+    )
+    suspend fun loadGoalById(id: Int): GoalEntity
 
     @Update
-    suspend fun updateUpperGoal(upperGoal: GoalEntity)
+    suspend fun updateGoal(upperGoal: GoalEntity)
+
+    @Query(
+        value = """
+            SELECT * FROM $TABLE_TODO WHERE $COL_UPPER_GOAL_ID = :upperGoalId 
+        """
+    )
+    suspend fun loadTodosByUpperGoalId(upperGoalId: Int): List<TodoEntity>
+
+    @Query(
+        value = """
+            SELECT * FROM $TABLE_GOAL WHERE $COL_UPPER_GOAL_ID = :upperGoalId 
+        """
+    )
+    suspend fun loadGoalsByUpperGoalId(upperGoalId: Int): List<GoalEntity>
 
     @Transaction
-    suspend fun checkTodo(todoEntity: TodoEntity, goalEntity: GoalEntity?) {
-        val newDone = todoEntity.done.not()
-        update(entity = todoEntity.copy(done = newDone))
-        goalEntity?.let {
-            updateUpperGoal(
-                upperGoal = goalEntity.copy(
-                    currentScore = goalEntity.currentScore + if (newDone) {
-                        todoEntity.upperGoalContributionScore ?: 0
-                    } else {
-                        -(todoEntity.upperGoalContributionScore ?: 0)
-                    }
+    suspend fun updateTodoTransaction(new: TodoEntity) {
+        val old = loadTodoById(new.todoId)
+
+        // 하위 먼저 업데이트 후
+        update(new)
+
+        // 예전 상위 업데이트
+        if (new.upperGoalId != old.upperGoalId) {
+            old.upperGoalId?.let { oldUpperGoalId ->
+                updateUpperGoalTransaction(oldUpperGoalId)
+            }
+        }
+
+        // 새로운 상위 업데이트
+        new.upperGoalId?.let { upperGoalId ->
+            updateUpperGoalTransaction(upperGoalId = upperGoalId)
+        }
+    }
+
+    @Transaction
+    suspend fun deleteTodoTransaction(deletedTodo: TodoEntity) {
+        delete(deletedTodo)
+
+        // 기존 상위 목표 삭제
+        deletedTodo.upperGoalId?.let { upperGoalId ->
+            updateUpperGoalTransaction(upperGoalId = upperGoalId)
+        }
+    }
+
+    @Transaction
+    suspend fun updateUpperGoalTransaction(upperGoalId: Int) {
+        val upperGoal = loadGoalById(upperGoalId)
+        val contributionTodos = loadTodosByUpperGoalId(upperGoalId)
+        val contributionGoals = loadGoalsByUpperGoalId(upperGoalId)
+        val todosScore = contributionTodos.sumOf {
+            if (it.done) {
+                it.upperGoalContributionScore ?: 0
+            } else {
+                0
+            }
+        }
+        val goalsScore = contributionGoals.sumOf {
+            if (it.done) {
+                it.upperGoalContributionScore ?: 0
+            } else {
+                0
+            }
+        }
+
+        // 하위 먼저 업데이트 후
+        updateGoal(
+            upperGoal.copy(
+                currentScore = todosScore + goalsScore
+            )
+        )
+
+        // 상위 업데이트
+        upperGoal.upperGoalId?.let {
+            updateUpperGoalTransaction(upperGoalId = it)
+        }
+    }
+
+    @Transaction
+    suspend fun checkTodoTransaction(todo: TodoEntity, upperGoal: GoalEntity?) {
+        val newDone = todo.done.not()
+        update(entity = todo.copy(done = newDone))
+
+        upperGoal?.let {
+            val newScore = upperGoal.currentScore + if (newDone) {
+                todo.upperGoalContributionScore ?: 0
+            } else {
+                -(todo.upperGoalContributionScore ?: 0)
+            }
+
+            updateGoal(
+                upperGoal = upperGoal.copy(
+                    currentScore = newScore
                 )
             )
+
+            processUpperGoalByTodoCheckTransaction(goal = upperGoal, newScore = newScore)
+        }
+    }
+
+    @Transaction
+    suspend fun processUpperGoalByTodoCheckTransaction(goal: GoalEntity, newScore: Int) {
+        // 상위 목표 있는지부터 체크
+        goal.upperGoalId?.let { upperGoalId ->
+            val upperGoal = loadGoalById(id = upperGoalId)
+
+            // 달성 -> 미달성
+            if (goal.done && goal.totalScore > newScore) {
+                val newUpperGoalScore =
+                    upperGoal.currentScore - (goal.upperGoalContributionScore ?: 0)
+                updateGoal(
+                    upperGoal = upperGoal.copy(
+                        currentScore = newUpperGoalScore
+                    )
+                )
+
+                processUpperGoalByTodoCheckTransaction(
+                    goal = upperGoal,
+                    newScore = newUpperGoalScore
+                )
+            }
+
+            // 미달성 -> 달성
+            if (goal.done.not() && goal.totalScore <= newScore) {
+                val newUpperGoalScore =
+                    upperGoal.currentScore + (goal.upperGoalContributionScore ?: 0)
+                updateGoal(
+                    upperGoal = upperGoal.copy(
+                        currentScore = newUpperGoalScore
+                    )
+                )
+
+                processUpperGoalByTodoCheckTransaction(
+                    goal = upperGoal,
+                    newScore = newUpperGoalScore
+                )
+            }
         }
     }
 }
