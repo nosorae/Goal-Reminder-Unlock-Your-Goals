bd3a0ade8ab7cc59c607b588758c6cced3f833ac
노소래 <yessorae@gmail.com>
2023-07-02 23:22:20 +0900

    feat: HiltWorker 어플리케이션 클래스에 셋업

--- app/src/main/java/com/yessorae/goalreminder/GoalReminderApplication.kt
+++ app/src/main/java/com/yessorae/goalreminder/GoalReminderApplication.kt
@@ -1,12 +1,18 @@
 package com.yessorae.goalreminder
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class GoalReminderApplication : Application() {
+class GoalReminderApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         initMobileAdmob()
@@ -22,4 +28,9 @@ class GoalReminderApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
     }
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
