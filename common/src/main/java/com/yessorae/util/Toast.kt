86387ee9f8b7f7144055fbd8b4d5031ecea8f7f4
노소래 <yessorae@gmail.com>
2023-07-05 22:28:46 +0900

    fix: 컨텍스트 확장함수 추가

--- common/src/main/java/com/yessorae/util/Toast.kt
+++
@@ -1,8 +0,0 @@
-package com.yessorae.util
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.showToast(stringModel: StringModel) {
-    Toast.makeText(this, stringModel.get(this), Toast.LENGTH_LONG).show()
-}
