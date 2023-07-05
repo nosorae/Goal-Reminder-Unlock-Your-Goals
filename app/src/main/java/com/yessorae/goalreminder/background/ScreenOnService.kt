256446cdb85ea24ee518d0ed896fb0756685874b
노소래 <yessorae@gmail.com>
2023-07-05 22:28:58 +0900

    fix: 포그라운드 노티 텍스트 수정, 누르면 액티비티 켜기

--- app/src/main/java/com/yessorae/goalreminder/background/ScreenOnService.kt
+++ app/src/main/java/com/yessorae/goalreminder/background/ScreenOnService.kt
@@ -5,7 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.IBinder
-import android.util.Log
+import com.yessorae.goalreminder.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
@@ -26,19 +26,16 @@ class ScreenOnService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.tag("SR-N").d("service onCreate")
         setupForegroundService()
         registerScreenOnOffReceiver()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Timber.tag("SR-N").d("service onStartCommand")
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Timber.tag("SR-N").d("service onDestroy")
         notification.cancel(this, FOREGROUND_SERVICE_NOTIFICATION_ID) // todo test
         unregisterReceiver(screenOnOffReceiver)
         job.cancel()
@@ -50,7 +47,12 @@ class ScreenOnService : Service() {
 
     private fun setupForegroundService() {
         notification.createForegroundServiceNotificationChannel(this)
-        val noti = notification.createForegroundNotification(this, "hi", "i am sorae").build()
+
+        val noti = notification.createForegroundNotification(
+            context = this,
+            title = getString(R.string.foreground_service_notification_title),
+            body = getString(R.string.foreground_service_notification_body)
+        ).build()
         startForeground(FOREGROUND_SERVICE_NOTIFICATION_ID, noti)
     }
 
