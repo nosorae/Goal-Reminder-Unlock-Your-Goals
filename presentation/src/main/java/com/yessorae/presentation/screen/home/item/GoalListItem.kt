dd180b78117d9fed545d761351608532f57134c2
노소래 <yessorae@gmail.com>
2023-07-10 17:01:00 +0900

    feat: 홈 리스트 아이템 아이콘 교체

--- presentation/src/main/java/com/yessorae/presentation/screen/home/item/GoalListItem.kt
+++ presentation/src/main/java/com/yessorae/presentation/screen/home/item/GoalListItem.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
@@ -152,7 +153,7 @@ fun GoalListItem(
             onClick = { onClickMore() }
         ) {
             Icon(
-                imageVector = Icons.Filled.Delete,
+                imageVector = Icons.Filled.MoreVert,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.onBackground
             )
--- presentation/src/main/java/com/yessorae/presentation/screen/home/item/TodoListItem.kt
+++ presentation/src/main/java/com/yessorae/presentation/screen/home/item/TodoListItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -125,7 +126,7 @@ fun TodoListItem(
             onClick = { onClickMore() }
         ) {
             Icon(
-                imageVector = Icons.Filled.Delete,
+                imageVector = Icons.Filled.MoreVert,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.onBackground
             )
