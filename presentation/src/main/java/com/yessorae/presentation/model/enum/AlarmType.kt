e73e02cd9bf63fbf9e6b6b451f6286a72100815f
노소래 <yessorae@gmail.com>
2023-07-14 00:24:54 +0900

    feat: AlarmType 추가

---
+++ presentation/src/main/java/com/yessorae/presentation/model/enum/AlarmType.kt
@@ -0,0 +1,39 @@
+package com.yessorae.presentation.model.enum
+
+import com.yessorae.presentation.R
+import com.yessorae.util.ResString
+import com.yessorae.util.StringModel
+import kotlinx.datetime.DateTimeUnit
+
+enum class AlarmType(val number: Int, val unit: DateTimeUnit, val display: StringModel) {
+    ONE_MINUTE(
+        number = 1,
+        unit =DateTimeUnit.MINUTE,
+        display = ResString(R.string.common_n_minute_ago, 1)
+    ),
+    TEN_MINUTE(
+        number = 10,
+        unit =DateTimeUnit.MINUTE,
+        display = ResString(R.string.common_n_minute_ago, 10)
+    ),
+    FIFTEEN_MINUTE(
+        number = 15,
+        unit =DateTimeUnit.MINUTE,
+        display = ResString(R.string.common_n_minute_ago, 15)
+    ),
+    THIRTY_MINUTE(
+        number = 30,
+        unit = DateTimeUnit.MINUTE,
+        display = ResString(R.string.common_n_minute_ago, 30)
+    ),
+    ONE_HOUR(
+        number = 1,
+        unit = DateTimeUnit.HOUR,
+        display = ResString(R.string.common_n_hour_ago, 1)
+    ),
+    ONE_DAY(
+        number = 1,
+        unit = DateTimeUnit.DAY,
+        display = ResString(R.string.common_n_day_ago, 1)
+    )
+}
