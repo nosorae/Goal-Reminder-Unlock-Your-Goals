a019154bd634da719d0f46a1ef7eeaadd26aec1f
노소래 <yessorae@gmail.com>
2023-07-18 23:24:41 +0900

    feat: 주기적알림에 사용될 워커 추가

---
+++ app/src/main/java/com/yessorae/goalreminder/background/worker/PeriodicNotificationWorker.kt
@@ -0,0 +1,40 @@
+package com.yessorae.goalreminder.background.worker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.yessorae.goalreminder.background.PeriodicNotificationManager
+import dagger.assisted.Assisted
+
+@HiltWorker
+class PeriodicNotificationWorker(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    val notificationManager: PeriodicNotificationManager
+) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
+        val title = inputData.getString(PARAM_TITLE) ?: DEFAULT_STRING
+        val body = inputData.getString(PARAM_SUBTITLE) ?: DEFAULT_STRING
+
+        notificationManager.apply {
+            createNotificationChannel(context = applicationContext)
+            showNotification(
+                context = applicationContext,
+                builder = createNotification(
+                    context = applicationContext,
+                    title = title,
+                    body = body
+                )
+            )
+        }
+
+        return Result.success()
+    }
+
+    companion object {
+        const val PARAM_TITLE = "title"
+        const val PARAM_SUBTITLE = "subtitle"
+        const val DEFAULT_STRING = ""
+    }
+}
