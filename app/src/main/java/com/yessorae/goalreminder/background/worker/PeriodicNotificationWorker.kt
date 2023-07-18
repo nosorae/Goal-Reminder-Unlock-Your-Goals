5de8eb477fc7e425010ce7efb2e3f0516edcca06
노소래 <yessorae@gmail.com>
2023-07-19 00:11:33 +0900

    fix: 주기적알림에 사용될 워커 빌드 에러 수정

--- app/src/main/java/com/yessorae/goalreminder/background/worker/PeriodicNotificationWorker.kt
+++ app/src/main/java/com/yessorae/goalreminder/background/worker/PeriodicNotificationWorker.kt
@@ -4,19 +4,22 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.yessorae.common.Logger
 import com.yessorae.goalreminder.background.PeriodicNotificationManager
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
 @HiltWorker
-class PeriodicNotificationWorker(
+class PeriodicNotificationWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted params: WorkerParameters,
-    val notificationManager: PeriodicNotificationManager
+    private val notificationManager: PeriodicNotificationManager
 ) : CoroutineWorker(context, params) {
     override suspend fun doWork(): Result {
         val title = inputData.getString(PARAM_TITLE) ?: DEFAULT_STRING
-        val body = inputData.getString(PARAM_SUBTITLE) ?: DEFAULT_STRING
+        val body = inputData.getString(PARAM_BODY) ?: DEFAULT_STRING
 
+        Logger.uiDebug("title $title, body $body")
         notificationManager.apply {
             createNotificationChannel(context = applicationContext)
             showNotification(
@@ -34,7 +37,7 @@ class PeriodicNotificationWorker(
 
     companion object {
         const val PARAM_TITLE = "title"
-        const val PARAM_SUBTITLE = "subtitle"
+        const val PARAM_BODY = "subtitle"
         const val DEFAULT_STRING = ""
     }
 }
